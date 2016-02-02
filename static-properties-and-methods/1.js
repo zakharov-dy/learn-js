e87cf4340835c7773a5f0fd3333a695c90b040e7
Dmitry <matrix.predictor@gmail.com>
2016-02-02 22:30:17 +0500

    Статические свойства

---
+++ static-properties-and-methods/1.js
@@ -0,0 +1,25 @@
+//Добавить в конструктор Article:
+//
+//   Подсчёт общего количества созданных объектов.
+//   Запоминание даты последнего созданного объекта.
+//   Используйте для этого статические свойства.
+//
+//   Пусть вызов Article.showStats() выводит то и другое.
+
+function Article() {
+   this.created = new Date();
+   Article.currentDate = this.created;
+   Article.count++;
+}
+Article.count = 0;
+Article.showStats = function(){
+   console.log("Всего: " + Article.count + ", Последняя: " + Article.currentDate);
+};
+new Article();
+new Article();
+
+Article.showStats(); // Всего: 2, Последняя: (дата)
+
+new Article();
+
+Article.showStats(); // Всего: 3, Последняя: (дата)
