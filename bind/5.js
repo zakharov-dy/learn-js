208063a27e6a2267fa9ebdfd3853017de722ce3d
Dmitry <matrix.predictor@gmail.com>
2016-02-10 22:19:13 +0500

    Добавлены задачки из главы bind

---
+++ bind/5.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function ask (question, answer, ok, fail) {
+   var result = prompt(question, '');
+   if (result.toLowerCase() == answer.toLowerCase()) ok();
+   else fail(); }
+var user = {   login: 'Василий',
+   password: '12345',
+   loginOk: function() {
+      alert( this.login + ' вошёл в сайт' );   },
+   loginFail: function() {
+      alert( this.login + ': ошибка входа' );   },
+   checkPassword: function() {
+      ask("Ваш пароль?", this.password, this.loginOk.bind(this), this.loginFail.bind(this));} };
+user.checkPassword();
