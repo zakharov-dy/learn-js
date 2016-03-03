aba0bea2a5dfd8f4cca0d35dd43271cc53eb9d70
zakharov-dy <matrix.predictor@gmail.com>
2016-03-03 19:15:18 +0500

    Добавляет ещё один файл  - repeat. В нем буду отписывать главы, которые хорошо бы почитать ещё раз. Или несколько раз

---
+++ learn_javascript_ru/oop/oop-errors/format-error.js
@@ -0,0 +1,33 @@
+//Создайте ошибку FormatError, которая будет наследовать от встроенного класса SyntaxError.
+//
+//    Синтаксис для её создания – такой же, как обычно:
+//
+//var err = new FormatError("ошибка форматирования");
+//
+//alert( err.message ); // ошибка форматирования
+//alert( err.name ); // FormatError
+//alert( err.stack ); // стек на момент генерации ошибки
+//
+//alert( err instanceof SyntaxError ); // true
+
+FormatError = function (message) {
+    SyntaxError.call(this);
+    this.message = message;
+    this.name = 'FormatError';
+    if (Error.captureStackTrace) {
+        Error.captureStackTrace(this, this.constructor);
+    } else {
+        this.stack = (new Error()).stack;
+    }
+};
+
+FormatError.prototype = Object.create(SyntaxError.prototype);
+FormatError.prototype.constructor = FormatError;
+
+var err = new FormatError("ошибка форматирования");
+
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // стек на момент генерации ошибки
+
+alert( err instanceof SyntaxError ); // true
---
+++ learn_javascript_ru/repeat.md
@@ -0,0 +1,3 @@
+## Главы, которые можно почитать повторно
+1. [Примеси](https://learn.javascript.ru/mixins)
+1. [Свои ошибки, наследование от Error](https://learn.javascript.ru/oop-errors)
